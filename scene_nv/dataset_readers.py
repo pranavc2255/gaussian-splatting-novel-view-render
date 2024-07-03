ca9e0e196b78cecdccfc6f3df344ea1a4fc9c315
pranavc2255 <122501043+pranavc2255@users.noreply.github.com>
2024-07-02 21:25:25 -0700

    Update dataset_readers.py

--- scene_nv/dataset_readers.py
+++ scene_nv/dataset_readers.py
@@ -13,7 +13,7 @@ import os
 import sys
 from PIL import Image
 from typing import NamedTuple
-from scene.colmap_loader import read_extrinsics_text, read_intrinsics_text, qvec2rotmat, \
+from scene_nv.colmap_loader import read_extrinsics_text, read_intrinsics_text, qvec2rotmat, \
     read_extrinsics_binary, read_intrinsics_binary, read_points3D_binary, read_points3D_text
 from utils.graphics_utils import getWorld2View2, focal2fov, fov2focal
 import numpy as np
