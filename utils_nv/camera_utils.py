73a03cdc7afe4e9e4e6976bc1a20e398e82566d3
pranavc2255 <122501043+pranavc2255@users.noreply.github.com>
2024-07-02 21:27:04 -0700

    Update camera_utils.py

--- utils_nv/camera_utils.py
+++ utils_nv/camera_utils.py
@@ -9,7 +9,7 @@
 # For inquiries contact  george.drettakis@inria.fr
 #
 
-from scene.cameras import Camera
+from scene_nv.cameras import Camera
 import numpy as np
 from utils.general_utils import PILtoTorch
 from utils.graphics_utils import fov2focal
